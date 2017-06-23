319ad52b406f2034177b61af3c9f56afcd228574
nwang9 <nicowang9@gmail.com>
2017-06-22 17:00:55 -0700

    created protype cell for inventory tracking table view

--- InventoryTrackingViewController.swift
+++ InventoryTrackingViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class InventoryTrackingViewController: UIViewController {
-
-    @IBAction func scanInventory(_ sender: Any) {
-            let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "inventoryScan") as UIViewController
-            self.present(viewController, animated: false, completion: nil)
+class InventoryTrackingViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+   // Store Inventory Data in cellContent
+    // let cellContent = [a:b]
+    
+    //Define rows etc.
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+        
     }
     
+    // Create cell
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "trackingCell")
+        
+        return cell
+    }
+    
+
+    // Send to scan page
+    @IBAction func scanInventory(sender: UIButton) {
+      //  let buttonPosition = (sender as AnyObject).convert(CGPoint.zero, to: self.tableView)
+        //let indexPath = self.tableView.indexPathForRow(at: buttonPosition)
+        
+        let invScanViewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "inventoryScan") as! ScanInventoryViewController
+        
+        
+            self.present(invScanViewController, animated: false, completion: nil)
+    }
+    // Send to trace page
     @IBAction func traceInventory(_ sender: Any) {
             let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "inventoryTrace") as UIViewController
             self.present(viewController, animated: false, completion: nil)
